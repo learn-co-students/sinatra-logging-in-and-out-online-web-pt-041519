633cd22a259a724c76cfa7d4da71ff51916aebfa
aparkening <aaron.parkening@gmail.com>
2019-07-30 13:25:10 -0400

    Done. User session validates and displays data.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
 class Helpers
+
+  # Find user in database; return user object
+  def self.current_user(session)
+    User.find_by(id: session[:user_id])
+  end
+
+  # Return true if user_id is in session hash
+  def self.is_logged_in?(session)
+    !!session[:user_id]
+  end
+
 end
