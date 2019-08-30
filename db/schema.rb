f72cffb24537fa58cbe611ca024f3b04b76d8a44
Jenna Ransom <jenna.ransom@gmail.com>
2019-08-30 13:57:42 -0600

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190815073144) do
+ActiveRecord::Schema.define(version: 20190830193342) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
